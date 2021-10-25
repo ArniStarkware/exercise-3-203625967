dc8329abbcd011ea890083777422b752e8427e80
ArniStarkware <arnon@starkware.co>
2021-10-25 17:08:19 +0300

    changin install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
-    python -m virtualenv .env --prompt "[exercise 3] "
+    python3 -m virtualenv .env --prompt "[exercise 3] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
