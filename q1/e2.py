7506eba2aa4507ed855e180720a56ef24164424a
ArniStarkware <arnon@starkware.co>
2021-10-26 12:17:41 +0300

    solved e2

--- q1/e2.py
+++ q1/e2.py
@@ -1,20 +1,21 @@
-from _typeshed import Self
 from os import listdir
 from os.path import isfile, isdir, join, basename
 
 class entry:
-    
     def __init__(self, path) -> None:
-        # self.path = path
         self.name = basename(path)
         if isdir(path):
-            self.type = 'dir' 
+            self.type = 'directory' 
         elif isfile(path):
             self.type = 'file'
         else:
             self.type = 'other'
-        if self.type == 'file':
-            self.skip = False
+        if self.type == 'directory':
+            self.skip_flag = False
+    
+    def skip(self) -> None:
+        if self.type == 'directory':
+            self.skip_flag = True
 
 def walk(root):
     queue = [join(root, file) for file in sorted(listdir(root))]
@@ -22,5 +23,5 @@ def walk(root):
         cur = queue.pop(0)
         ent = entry(cur)
         yield ent
-        if ent.type == 'file':
-            queue.append([join(cur, file) for file in sorted(listdir(root))])
+        if ent.type == 'directory' and not ent.skip_flag:
+            queue += [join(cur, file) for file in sorted(listdir(cur))]
