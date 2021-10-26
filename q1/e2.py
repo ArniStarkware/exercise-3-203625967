92e0c44f169962ce3d60bf78ef396e07c97e4853
ArniStarkware <arnon@starkware.co>
2021-10-26 11:34:00 +0300

    before pytest

--- q1/e2.py
+++ q1/e2.py
@@ -1,2 +1,26 @@
+from _typeshed import Self
+from os import listdir
+from os.path import isfile, isdir, join, basename
+
+class entry:
+    
+    def __init__(self, path) -> None:
+        # self.path = path
+        self.name = basename(path)
+        if isdir(path):
+            self.type = 'dir' 
+        elif isfile(path):
+            self.type = 'file'
+        else:
+            self.type = 'other'
+        if self.type == 'file':
+            self.skip = False
+
 def walk(root):
-    pass # TODO
+    queue = [join(root, file) for file in sorted(listdir(root))]
+    while queue:
+        cur = queue.pop(0)
+        ent = entry(cur)
+        yield ent
+        if ent.type == 'file':
+            queue.append([join(cur, file) for file in sorted(listdir(root))])
